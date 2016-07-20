70ac81eb4d7a0c5c93b8371d469a00a587807cbb
Samantha Klein <saradurga@gmail.com>
2016-07-20 15:47:32 -0500

    initial, problematic routes

---
+++ server/public/scripts/controllers/comparecontroller.js
@@ -0,0 +1,26 @@
+myApp.controller('CompareController', ['$scope', '$http', function($scope, $http) {
+
+  console.log('compare controller running');
+
+
+var key = '&api_key=d6a31734ac6f3e40367ee50308f299ad';
+var baseURL = 'http://api.themoviedb.org/3/search/person?query=';
+
+getCrowe();
+
+
+function getCrowe() {
+  var crowe = 'russell crowe';
+  var query = baseURL + crowe + key;
+    console.log(query);
+
+
+    $http.jsonp(query).then(
+      function(response) {
+        console.log(response.data);
+        $scope.crowe = response.data;
+      }
+    )
+  }
+
+  }]);
---
+++ server/routes/data.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+
+//Routes
+
+
+module.exports = router;
